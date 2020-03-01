649fb2019810e12659b1492455f3879d352a5186
Basavaraju S R <srbasavaraju@users.noreply.github.com>
2020-03-01 16:24:54 +0530

    updating the quick sort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>();
+            Console.WriteLine("Quick Sort");
+            Console.Write("-----------------------------------------\n");
+            Console.WriteLine("Please enter the array of integers ");
+
+            string[] stringArr = Console.ReadLine().Split(' ');
+
+            foreach (string str in stringArr)
+            {
+                arr.Add(Convert.ToInt32(str));
+            }
+
+            QuickSort quickSort = new QuickSort();
+            int[] sortedArr = quickSort.Sort(arr.ToArray(), 0, arr.Count-1);
+
+            Console.WriteLine("Sorted Array");
+            foreach (int item in sortedArr)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,52 @@
+﻿namespace QuickSort
+{
+    class QuickSort
+    {
+        internal int[] Sort(int[] arr, int lower, int upper)
+        {
+            if (upper > lower)
+            {
+                int pivot = Split(arr, lower, upper);
+                if (pivot > 1)
+                {
+                    Sort(arr, lower, pivot - 1);
+                }
+                if (pivot + 1 < upper)
+                {
+                    Sort(arr, pivot + 1, upper);
+                }
+            }
+            return arr;
+        }
+
+        private static int Split(int[] arr, int lower, int upper)
+        {
+            int pivot = arr[lower];
+            while (true)
+            {
+                while (arr[lower] < pivot)
+                {
+                    lower++;
+                }
+
+                while (arr[upper] > pivot)
+                {
+                    upper--;
+                }
+
+                if (lower < upper)
+                {
+                    if (arr[lower] == arr[upper]) return upper;
+
+                    int temp = arr[lower];
+                    arr[lower] = arr[upper];
+                    arr[upper] = temp;
+                }
+                else
+                {
+                    return upper;
+                }
+            }
+        }
+    }
+}
